3b30b6b0b4172314e24e88d7a5ea9b417a235798
Alex-Au1 <45087631+Alex-Au1@users.noreply.github.com>
2019-05-19 04:43:23 -0400

    Finished window and added character
    -Added Relationship with inputController ---> able to handle inputs
    -Finished Overall game window design

--- gameWindow.java
+++ gameWindow.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
 
 import javax.swing.JFrame;
@@ -18,11 +19,20 @@ public class gameWindow extends JFrame{
 	int windowHeight = 900;
 	
 	//*********temporary start position of the moving circle******* DELETE LATER
-	int x = 0;
+	int x = 450;
 	
 	//memory of the image of the previous frame
 	Image buffer = new BufferedImage(windowWidth, windowHeight, BufferedImage.TYPE_INT_RGB);
 	
+	//border for the window
+	Insets border = this.getInsets();
+	
+	
+	
+	//object for handling the inputs of the user
+	inputController input = new inputController(this);
+	
+		
 	
 	public static void main(String[] args) {
 		gameWindow game = new gameWindow();
@@ -40,9 +50,17 @@ public class gameWindow extends JFrame{
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setVisible(true);
 		setResizable(false);
+		
+		//set the size of the border
+		setSize(border.left + windowWidth + border.right, border.top + windowHeight + border.bottom);
 	}
 	
-	public void run() {
+	
+	/*
+	 * -makes the window for the game
+	 * -Continuously updates the game
+	 */
+	void run() {
 		makeWindow();
 		
 		while(isRunning) {
@@ -67,11 +85,24 @@ public class gameWindow extends JFrame{
 		
 	}
 	
-	public void update() {
-		x=x+2;
+	void update() {
+		//moves the character/box
+		//moves the character right
+		if (input.keyDown(KeyEvent.VK_RIGHT)) {
+			x+=3;
+		}
+		
+		if (input.keyDown(KeyEvent.VK_LEFT)) {
+			x-=3;
+		}
 	}
 	
-	public void draw() {
+	
+	
+	/*
+	 * -draws the sprites and images for the game
+	 */
+	void draw() {
 		Graphics printImage = getGraphics();
 		Graphics prevImage = buffer.getGraphics();
 		
@@ -83,11 +114,11 @@ public class gameWindow extends JFrame{
 		prevImage.setColor(Color.orange);
 		prevImage.fillRect(0, windowHeight-300, windowWidth, 300);
 		
-		//moving white circle
+		//character
 		prevImage.setColor(Color.orange);
 		prevImage.fillRect(x, windowHeight-350, 50, 50);
 		
-		printImage.drawImage(buffer,0,0,this);
+		printImage.drawImage(buffer, border.left, border.top, this);
 		
 	}
 }
