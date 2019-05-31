f15af2097bb65de1c55b3c95f0f6d9c0a5ca629a
Alex-Au1 <45087631+Alex-Au1@users.noreply.github.com>
2019-05-31 02:05:00 -0400

    Add files via upload

---
+++ src/gameWindow.java
@@ -0,0 +1,44 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class gameWindow extends Canvas {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3431428519140837998L;
+	
+	//window
+	JFrame windowGUI;
+	
+	//constructor to create the JFrame
+	public gameWindow(int width, int height, String title, game boxGame) {
+		//window for the game
+		windowGUI = new JFrame(title);
+		//////test button////////
+		JButton startButton = new JButton("Start");
+		
+		/////dimensions for the test button////////
+		startButton.setPreferredSize(new Dimension(100,100));
+		startButton.setBounds(120,150,100,100);
+		startButton.setBackground(new Color(255,255,255));
+		
+		//attributes for the window
+		windowGUI.setPreferredSize(new Dimension(width,height));
+		windowGUI.setMaximumSize(new Dimension(width, height));
+		windowGUI.setMinimumSize(new Dimension(width, height));
+		
+		windowGUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		windowGUI.setResizable(false);
+		windowGUI.setLocationRelativeTo(null);
+		
+		//windowGUI.add(startButton);
+		windowGUI.add(boxGame);
+		windowGUI.setVisible(true);
+		boxGame.start();
+	}
+	
+	public JFrame getWindow() {
+		return windowGUI;
+	}
+}
