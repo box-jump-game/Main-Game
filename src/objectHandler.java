3787140cd88bbbe72557e5784237707dcaa7a5ff
Alex-Au1 <45087631+Alex-Au1@users.noreply.github.com>
2019-06-10 05:52:44 -0400

    Update objectHandler.java

--- src/objectHandler.java
+++ src/objectHandler.java
@@ -3,69 +3,60 @@ import java.util.LinkedList;
 import java.util.Random;
 
 public class objectHandler {
-	
-	//linked list that stores all the objects
+
+	// linked list that stores all the objects
 	LinkedList<gameObjects> objectList = new LinkedList<gameObjects>();
-	
-	//random number
+
+	// random number
 	Random r = new Random();
 	
-	//spawn rate of the obstacles
-	int obstacleSpawnTime = 2000;
-	
-	//updates the action of the object
+	gameStats score;
+
+	public objectHandler(gameStats score) {
+		this.score = score;
+	}
+
+	// updates the action of the object
 	public void update() {
 		for (int i = 0; i < objectList.size(); i++) {
 			objectList.get(i).updateObject();
 		}
 	}
-	
-	//renders all the objects
+
+	// renders all the objects
 	public void render(Graphics2D g2D) {
-		
+
 		for (int i = 0; i < objectList.size(); i++) {
+
 			objectList.get(i).renderObject(g2D);
-			
-			//if the snow objects go out of the screen, create a new snow object
-			if (objectList.get(i).getName().equals(objectType.SNOW)) {
-				if (objectList.get(i).getLocationX() > game.WIDTH || objectList.get(i).getLocationY() > game.HEIGHT || objectList.get(i).getLocationX() < 0 || objectList.get(i).getLocationY() < 0) {
-					removeObject(objectList.get(i));
-					addObject(0, new snow(objectType.SNOW, r.nextInt(game.WIDTH), r.nextInt(game.HEIGHT)/2, System.currentTimeMillis(), false));
-				}
-				
-			}
-			
-			//if the object is a tree obstacle
+	
+			// if the object is a tree obstacle
 			if (objectList.get(i).getName().equals(objectType.TREE)) {
 				
-				//creates a new tree depending on the spawn rate
-				if (System.currentTimeMillis() - objectList.get(i).getDrawnTime() > obstacleSpawnTime && objectList.get(i).getAdd()) {
-					objectList.get(i).setAdd(false);
-					System.out.println("create");
-					addObject(new obstacle(objectType.TREE, game.WIDTH, game.HEIGHT - ground.height - obstacle.height, System.currentTimeMillis(), true));
-				}
-				
-				//removes the tree obstacle if its x-position crosses the left side of the JFrame
-				if (objectList.get(i).getLocationX() < 0 - obstacle.width) {
-					System.out.println("remove");
-					removeObject(objectList.get(i));
+				//stops the game if the player hits an obstacle
+				if (objectList.get(game.snowFlakes + 1).getLocationX() + player.width > objectList.get(i).getLocationX() && objectList.get(game.snowFlakes + 1).getLocationX() < objectList.get(i).getLocationX() + obstacle.width  && objectList.get(game.snowFlakes + 1).getLocationY() + player.height > objectList.get(i).getLocationY()) {
+					//System.out.println("obstacleX: " +  (objectList.get(i).getLocationX() + obstacle.width) + " playerX: " + objectList.get(201).getLocationX());
+					for (int j = 0; j < objectList.size(); j++) {
+						objectList.get(j).setSpeedX(0);
+						objectList.get(j).setSpeedY(0);
+					}
 				}
 			}
 		}
 	}
-	
-	//adds objects into the object list
+
+	// adds objects into the object list
 	public void addObject(gameObjects object) {
 		this.objectList.add(object);
 	}
-	
+
 	public void addObject(int index, gameObjects object) {
 		this.objectList.add(index, object);
 	}
-	
-	//removes objects from the object list
+
+	// removes objects from the object list
 	public void removeObject(gameObjects object) {
 		this.objectList.remove(object);
 	}
-	
+
 }
